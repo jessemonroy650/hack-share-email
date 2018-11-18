5663cd9084a4dc6539011be2df15ee2c18f3c2e8
jessemonroy650 <jessemonroy650@yahoo.com>
2018-11-18 03:02:03 -0700

    initial entry

---
+++ js/app.js
@@ -0,0 +1,38 @@
+//
+//
+//
+var app = {
+    version : '1.0.0',
+    targetEvent : 'click',
+    isCordova   : false,
+
+    hook : function () {
+        document.getElementById('shareButton').addEventListener(app.targetEvent, function () {
+            console.log('#shareButton');
+            document.getElementById('test').innerHTML = '#shareButton';
+        });
+    },
+    //
+    onDOMContentLoaded : function () {
+        //alert("onDOMContentLoaded");
+        document.getElementById('appIcon').src    = 'img/bellpepper.png';
+        document.getElementById('test').innerHTML = 'app.onDOMContentLoaded';
+        //
+        app.targetEvent = 'click';
+        app.isCordova   = (typeof window.cordova !== "undefined");
+        //
+        app.hook();
+    },
+    //
+    onDeviceReady : function () {
+        document.getElementById('appIcon').src    = 'img/apple.png';
+        document.getElementById('test').innerHTML = 'app.onDeviceReady';
+
+        app.targetEvent = 'touchend';
+        app.isCordova   = (typeof window.cordova !== "undefined");
+        //
+        cordova.plugins.email.isAvailable(function isAvailable() {
+            document.getElementById('mailPlugin').innerHTML = isAvailable;
+        });
+    }
+};
